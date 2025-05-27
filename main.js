fc1a9feac094e08c3b7175090dcb7e4dbb70db41
CurtMinyard <cminyard@perseverenow.org>
2025-05-27 11:16:59 -0500

    Initial Commit

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
 /******************
  * YOUR CODE HERE *
- ******************
+ *******************/
 
-
+function getFirstItemFrom(arr) {
+  return arr[0];
+}
+function getLastItemFrom(arr) {
+  return arr[arr.length - 1];
+}
+function getIndex3(arr) {
+  return arr.length > 3 ? arr[3] : arr[arr.length - 1];
+}
+function isLongList(arr) {
+  return arr.length >= 10;
+}
+function firstItemIsNumber(arr) {
+  return typeof arr[0] === 'number';
+}
+function secondCharOfThirdString(arr) {
+  const thirdItem = arr[2];
+  if (typeof thirdItem === 'string' && thirdItem.length >= 2) {
+    return thirdItem[1];
+  }
+  return undefined; 
+}
 
 
  /********************************************
